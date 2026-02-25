78dface14b3255929c1c214b403e2127bb9c7288
eosca <e.oscamartinezcorb@edu.gva.es>
2026-02-25 19:14:14 +0100

    Add new article first approach: console.log with article data when pushing create button

--- src/views/CreateArticleView.vue
+++ src/views/CreateArticleView.vue
@@ -1,22 +1,35 @@
 <script setup>
+import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 const router = useRouter();
 const route = useRoute();
 
-// console.log("router: ", router);
-// console.log("route: ", route);
+const title = ref("");
+const description = ref("");
+const price = ref(0);
 </script>
 <template>
   <h1>Crear artículo</h1>
   <div class="article__container">
     <div class="article__title">
-      <v-text-field label="Título"></v-text-field>
+      <v-text-field label="Título" v-model="title"></v-text-field>
     </div>
-    <div class="article__description"></div>
-    <div class="article__price"></div>
+    <div class="article__description">
+      <v-textarea label="Descripción" v-model="description"></v-textarea>
+    </div>
+    <div class="article__price">
+      <v-number-input control-variant="split" v-model="price"></v-number-input>
+    </div>
   </div>
 
-  <!-- <p><strong>Current route path:</strong> {{ $route.fullPath }}</p> -->
+  <button @click="console.log({ title, description, price })">Crear</button>
   <button @click="$router.push({ name: 'home' })">Volver</button>
 </template>
+
+<style scoped>
+.article__container {
+  margin: 20px;
+  max-width: 300px;
+}
+</style>
